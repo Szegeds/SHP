44d1b2b413ce8da1e2410709fdd66dc0ce339de5
Szegeds <ked_3177@hotmail.co.th>
2017-06-16 14:10:33 +0700

    sdf

--- pages/admin/product1.php
+++ pages/admin/product1.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="">   
     <link rel="icon" href="../../favicon.ico">
 
     <title>รายการสินค้่า</title>
