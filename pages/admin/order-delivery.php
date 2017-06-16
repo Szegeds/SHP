0275ff93d5a28629588494c0c666ec2dcea62fe9
sen <potherdash@hotmail.com>
2017-06-16 14:24:05 +0700

    sender

--- pages/admin/order-delivery.php
+++ pages/admin/order-delivery.php
@@ -2,7 +2,7 @@
 include "check-login.php";
 include "dblink.php";
 sleep(1);    
-$order_id = $_POST['order_id'];
+$order_id = $_POST['order_id'];   
 $sql = "UPDATE orders SET delivery = 'yes' WHERE order_id = '$order_id'";
 mysqli_query($link, $sql);
 
