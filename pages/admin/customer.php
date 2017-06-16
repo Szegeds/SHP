053def2bd11470791d07bd8f085a067757801cc1
sen <potherdash@hotmail.com>
2017-06-16 17:17:06 +0700

    change UI customer.php

--- pages/admin/customer.php
+++ pages/admin/customer.php
@@ -1,5 +1,6 @@
 <?php 
-include "check-login.php";
+include "../include/include_web.php";
+include "../include/check-login.php"; 
  ?>
 <!doctype html>
 <html>
@@ -7,53 +8,77 @@ include "check-login.php";
 <meta charset="utf-8">
 <title>Data Store</title>
 <style>
-	@import "global.css";
+	table th {
+		background: #bf80ff;
+		color: #fff;
+		border: solid 1px #bf80ff;
+	}
+	tr:nth-of-type(odd) {
+		background: #fff;
+	}
+	tr:nth-of-type(even) {
+		background: #fff;
+	}
+	td {
+/* 		vertical-align: top; */
+/* 		padding: 3px 0px 3px 5px; */
+		border: solid 1px #bf80ff;
+	}
+	td:first-child, td:first-child {
+		text-align: center;
+	}
+	table tr:hover td {
+		color: #fff;
+		background: #bf80ff;
+		border: solid 1px #fff;
+		
+	}
 	caption {
 		text-align: left;
 		padding-bottom: 3px;
 	}
-	td:nth-child(1) {
-		width: 50px;
-	}
-	td:nth-child(2) {
-		width: 180px;
-	}
-	td:nth-child(3) {
-		width: 200px;
-	}
-	td:nth-child(4) {
-		width: 100px;
-	}
-	td:nth-child(5) {
-		width: 220px;
-	}
-	td:nth-child(6) {
-		width: 50px;
-	}
-	table th {
-		background: green;
-		color: yellow;
-		padding: 5px;
-		border-right: solid 1px white;
-		font-size:12px;
-	}
-	tr:nth-of-type(odd) {
-		background: lavender;
-	}
-	tr:nth-of-type(even) {
-		background: whitesmoke;
-	}
-	td {
-		vertical-align: top;
-		padding: 3px 0px 3px 5px;
-		border-right: solid 1px white;
-	}
-	td:first-child, td:last-child {
-		text-align: center;
-	}
-	td a:hover {
-		color: red;
-	}
+/* 	td:nth-child(1) { */
+/* 		width: 50px; */
+/* 	} */
+/* 	td:nth-child(2) { */
+/* 		width: 180px; */
+/* 	} */
+/* 	td:nth-child(3) { */
+/* 		width: 200px; */
+/* 	} */
+/* 	td:nth-child(4) { */
+/* 		width: 100px; */
+/* 	} */
+/* 	td:nth-child(5) { */
+/* 		width: 220px; */
+/* 	} */
+/* 	td:nth-child(6) { */
+/* 		width: 50px; */
+/* 	} */
+/* 	table th { */
+/* 		background: green; */
+/* 		color: yellow; */
+/* 		padding: 5px; */
+/* 		border-right: solid 1px white; */
+/* 		font-size:12px; */
+/* 	} */
+/* 	tr:nth-of-type(odd) { */
+/* 		background: lavender; */
+/* 	} */
+/* 	tr:nth-of-type(even) { */
+/* 		background: whitesmoke; */
+/* 	} */
+/* 	td { */
+/* 		vertical-align: top; */
+/* 		padding: 3px 0px 3px 5px; */
+/* 		border-right: solid 1px white; */
+/* 	} */
+/* 	td:first-child, td:last-child { */
+/* 		text-align: center; */
+/* 	} */
+/* 	td a:hover { */
+/* 		color: red; */
+/* 	} */
 	p#pagenum {
 		width: 90%;
 		text-align: center;
@@ -71,7 +96,7 @@ include "check-login.php";
 <script src="js/jquery.blockUI.js"> </script>
 <script>
 $(function() {	
-	$('button.del').click(function() {
+	$('a.del').click(function() {
 		if(!(confirm("ยืนยันการลบลูกค้ารายนี้"))) {
 			return;
 		}
@@ -98,11 +123,17 @@ function ajaxSend(dataJSON) {
 </script>
 </head>
 
-<body><?php include "top.php"; ?>
+<body>
+<?php include "../include/header.php"; ?>
+
+ <div class="container-fluid">
+      <div class="row row-offcanvas row-offcanvas-right">
+		<?php include "../include/side_menu.php"; ?>
+        <div class="col-12 col-md-9">
 <article>
 <?php
-include "dblink.php";
-include "lib/pagination.php";
+include "../include/dblink.php";
+include "../../lib/pagination.php";
 
 $sql = "SELECT * FROM customers";
 $result = page_query($link, $sql, 20);
@@ -119,6 +150,8 @@ if($total == 0) {
     <form>
     	<input name="kw">&nbsp;
         <button onClick="javascript: alert('ลองทำด้วยตนเอง')">ค้นหา</button>
+        <a class="" id="<?php echo $cust['cust_id']; ?>" href=""><span class="fa fa-search"></span></a>
+        
      </form>
 </caption>
 <tr><th>ลำดับ</th><th>ชื่อลูกค้า</th><th>ที่อยู่</th><th>โทร</th><th>อีเมล</th><th>คำสั่ง</th></tr>
@@ -133,7 +166,7 @@ while($cust = mysqli_fetch_array($result)) {
     <td><?php echo $cust['phone']; ?></td>
     <td><a href="mailto:<?php echo $cust['email']; ?>"><?php echo $cust['email']; ?></a></td>
     <td>
-     		<button class="del" data-id="<?php echo $cust['cust_id']; ?>">ลบ</button>
+     		<a class="del" id="<?php echo $cust['cust_id']; ?>" href=""><span class="fa fa-trash-o"></span></a>
     </td>
 </tr>
 <?php
@@ -150,5 +183,6 @@ if(page_total() > 1) { 	 //ให้แสดงหมายเลขเพจ�
 ?>
 
 </article>
+</div></div>
 </body>
 </html>
