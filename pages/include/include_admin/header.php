177d816782afc212eb95a1041c95ad615aa14a27
Szegeds <ked_3177@hotmail.co.th>
2017-06-18 15:14:56 +0700

    delete include

--- pages/include/include_admin/header.php
+++ pages/include/include_admin/header.php
@@ -1,6 +1,15 @@
-<?php
-include "../include_web.php";
-?>
+<link rel="stylesheet" href="../../../_assets/bootstrap-4.0.0-alpha.6/dist/css/bootstrap-grid.min.css">
+<link rel="stylesheet" href="../../../_assets/bootstrap-4.0.0-alpha.6/dist/css/bootstrap-reboot.min.css">
+<link rel="stylesheet" href="../../../_assets/bootstrap-4.0.0-alpha.6/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="../../../_assets/font-awesome-4.7.0/css/font-awesome.min.css">
+
+
+<!-- <script src="../../js/jquery-3.2.1.min.js"></script> -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="../../../_assets/tether-master/dist/js/tether.min.js"></script>
+<script src="../../../_assets/bootstrap-4.0.0-alpha.6/dist/js/bootstrap.min.js"></script>
+
 <style>
 .bg-lavender{
 background-color:#FFF;
